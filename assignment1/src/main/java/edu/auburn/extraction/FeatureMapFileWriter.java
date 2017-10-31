0f00870e83fc10015650286fcfc6ad06524cf737
rLegacy <31908488+rLegacy@users.noreply.github.com>
2017-10-31 13:25:08 -0500

    Update FeatureMapFileWriter.java

--- assignment1/src/main/java/edu/auburn/extraction/FeatureMapFileWriter.java
+++ assignment1/src/main/java/edu/auburn/extraction/FeatureMapFileWriter.java
@@ -21,7 +21,7 @@ public class FeatureMapFileWriter {
   }
   
   public void writeVectors(String url, FeatureMap<Character> featureMap) throws IOException {
-    getFileWriter().write(featureMap.toString());
+    getFileWriter().write(featureMap.toStringVector());
     getFileWriter().write("\n");
   }
   
